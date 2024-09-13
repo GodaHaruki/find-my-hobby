f2b728ca33adca902914a2f7f041911dc163158b
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2024-09-13 14:45:19 +0000

    add result page

--- test3/src/main.tsx
+++ test3/src/main.tsx
@@ -3,9 +3,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Result from './Result.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "result",
+    element: <Result />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
