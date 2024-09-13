fdc4672051dba03d77b1b671fd232e570a6fdad9
ShojiYuya <factory.music8888@gmail.com>
2024-09-13 14:39:36 +0000

    add new lib

--- test3/src/main.tsx
+++ test3/src/main.tsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
