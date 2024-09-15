dd19323936e995c35c62f146ee1f3c9b06364e27
ShojiYuya <factory.music8888@gmail.com>
2024-09-15 15:18:56 +0000

    mini changes3

--- test3/src/App.tsx
+++ test3/src/App.tsx
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Question from './question';
+
+
+
 function Home() {
-return
+return(
 <Router>
+<div>
+    <nav>
+        <ul>
+            <h1>  </h1>
+            <li><Link to="/question[1]">ここから始める</Link></li>
+            <li><Link to="/request">お問い合わせ</Link></li>
+        </ul>
+    </nav>
 
+    <Routes>
+        <Route path="/puestion[1]" element={<question />}/>
+        <Route path="/request" element={<request />}/>
+    </Routes>
+</div>
 </Router>
-
-
+);
 }
+export default Home;
