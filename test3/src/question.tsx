710be6466a89fac5485f1d5ba793f892e2903ff0
ShojiYuya <factory.music8888@gmail.com>
2024-09-15 16:17:30 +0000

    簡単なフォーム

---
+++ test3/src/question.tsx
@@ -0,0 +1,11 @@
+function Q() {
+return(
+   <>
+   <h1>What kind of do you like the category of under lists?</h1>
+   </>
+);
+
+
+
+}
+export default Q;
---
+++ test3/src/request.tsx
@@ -0,0 +1,9 @@
+function R() {
+    return(
+       <>
+       <h1>If you feel bad, please write something of the points which let us improve ?</h1>
+       </>
+
+    );
+    }
+    export default R;
