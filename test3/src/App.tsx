224347a356b7fc59728ccc145bf7449d7ff0d8c2
ShojiYuya <factory.music8888@gmail.com>
2024-09-15 15:36:49 +0000

    mini changes4

--- test3/src/App.tsx
+++ test3/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Question from './question';
-
+import Request from './request';
 
 
 function Home() {
@@ -16,8 +16,8 @@ return(
     </nav>
 
     <Routes>
-        <Route path="/puestion[1]" element={<question />}/>
-        <Route path="/request" element={<request />}/>
+        <Route path="/puestion[1]" element={<Question />} />
+        <Route path="/request" element={<Request />} />
     </Routes>
 </div>
 </Router>
